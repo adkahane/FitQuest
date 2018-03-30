c648defce81c91a26b300ced54ba43894a979507
psharif <psharif@asu.edu>
2018-03-29 19:29:01 -0700

    Adding Code For Expo App

--- components/FitMap/FitMap.js
+++ components/FitMap/FitMap.js
@@ -1,46 +1,70 @@
-import React from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-import MapView, { Polyline } from 'react-native-maps';
+import React, { Component } from 'react';
+import { Platform, Text, View, StyleSheet, Dimensions} from 'react-native';
+import { Constants, Location, Permissions, MapView } from 'expo';
 import styles from './MapStyles.js';
+let { width, height } = Dimensions.get('window');
+
+const ASPECT_RATIO = width / height;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 class FitMap extends React.Component {
+
+  state = {
+    location: {
+      "coords": {
+        "accuracy": 65,
+        "altitude": 54.543243408203125,
+        "altitudeAccuracy": 10,
+        "heading": -1,
+        "latitude": 37.871732795815525,
+        "longitude": -122.27066792384305,
+        "speed": -1,
+      },
+      "timestamp": 1522374444583.582,
+    },
+    errorMessage: null,
+  };
+
+  componentWillMount() {
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+      });
+    } else {
+      this._getLocationAsync();
+    }
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+
+    let location = await Expo.Location.watchPositionAsync(
+      {enableHighAccuracy: true, distanceInterval: 5},
+      (location)=> {
+        console.log(location);
+        this.setState({location})
+      }
+    );
+  };
+
   render() {
     return (
-      <MapView
-        style={styles.container} 
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }} >
-        <Polyline
-        coordinates={[
-          { latitude: 37.8025259, longitude: -122.4351431 },
-          { latitude: 37.7896386, longitude: -122.421646 },
-          { latitude: 37.7665248, longitude: -122.4161628 },
-          { latitude: 37.7734153, longitude: -122.4577787 },
-          { latitude: 37.7948605, longitude: -122.4596065 },
-          { latitude: 37.8025259, longitude: -122.4351431 }
-        ]}
-        strokeColor="#000" // fallback for when `strokeColors` is not supported by the map-provider
-        strokeColors={[
-          '#7F0000',
-          '#00000000', // no color, creates a "long" gradient between the previous and next coordinate
-          '#B24112',
-          '#E5845C',
-          '#238C23',
-          '#7F0000'
-        ]}
-        strokeWidth={6}
-      />
-      </MapView>
+          <MapView
+            provider={ MapView.PROVIDER_GOOGLE }
+            style={ styles.container }
+            showsUserLocation={ true }
+            region={
+              {latitude: this.state.location.coords.latitude, 
+               longitude: this.state.location.coords.longitude, 
+               latitudeDelta: LATITUDE_DELTA, 
+               longitudeDelta: LONGITUDE_DELTA }
+            }/>  
     );
   }
 }
