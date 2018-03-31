a9ad0d73a4d088a4b96cfb57f5fbc17f7ccd6f5d
psharif <psharif@asu.edu>
2018-03-30 17:45:47 -0700

    Changed A Bit of Stuff

--- components/FitMap/FitMap.js
+++ components/FitMap/FitMap.js
@@ -5,17 +5,15 @@ import styles from './MapStyles.js';
 let { width, height } = Dimensions.get('window');
 
 const ASPECT_RATIO = width / height;
-const LATITUDE_DELTA = 0.00522;
+const LATITUDE_DELTA = 0.00322;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 class FitMap extends React.Component {
-
 	state = {
 		quest: {
 			polylines: [],
 			speed: [],
-			timestamp: [],
-			polyId: 5 
+			timestamp: []
 		},
 		location: {
 			latitude:  37.871732795815525,
@@ -53,24 +51,23 @@ class FitMap extends React.Component {
 	};
 
 	render() {
-		 const stuff = this.state.quest.coordinates;
 		return (
-					<MapView
-						provider={ MapView.PROVIDER_GOOGLE }
-						style={ styles.container }
-						showsUserLocation={ true }
-						region={
-							{latitude: this.state.location.latitude, 
-							 longitude: this.state.location.longitude, 
-							 latitudeDelta: LATITUDE_DELTA, 
-							 longitudeDelta: LONGITUDE_DELTA }
-						}> 
-								<MapView.Polyline
-									coordinates={this.state.quest.polylines.map((polyline)=>polyline)}
-									strokeColor="#000"
-									strokeWidth={3}
-								/>
-						</MapView>
+			<MapView
+				provider={ MapView.PROVIDER_GOOGLE }
+				style={ styles.container }
+				showsUserLocation={ true }
+				region={
+					{latitude: this.state.location.latitude, 
+					 longitude: this.state.location.longitude, 
+					 latitudeDelta: LATITUDE_DELTA, 
+					 longitudeDelta: LONGITUDE_DELTA }
+				}> 
+					<MapView.Polyline
+						coordinates={this.state.quest.polylines.map((polyline)=>polyline)}
+						strokeColor="#000"
+						strokeWidth={3}
+					/>
+			</MapView>
 		);
 	}
 }
