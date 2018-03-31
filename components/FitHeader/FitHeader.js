c172ed437204c9af962c859d2d530e6d76bd30c4
Brandan Richardson <brandanr@gmail.com>
2018-03-31 15:27:08 -0700

    ScrollViews added to FitStats and FitHome

--- components/FitHeader/FitHeader.js
+++ components/FitHeader/FitHeader.js
@@ -4,22 +4,16 @@ import { Header, Card } from 'react-native-elements';
 class FitHeader extends React.Component {
   render() {
     return (
-       <Card containerStyle={{flexDirection: 'row', width: "100%",
-        height: '10%', backgroundColor: 'rgba(49, 111,244, 1)',
-        borderColor: 'rgba(49, 111,244, 1)', justifyContent: 'space-around',
-        alignItems: 'stretch', marginTop: '0%'
-        }}> 
                 
           <Header 
             statusBarProps={{ barStyle: 'light-content' }}
             centerComponent={{ text: 'FitQuest', style: { color: '#fff',
-            fontWeight: 'bold', fontSize: 30, marginTop: '-3%', marginBottom: '6%' } }}
+            fontWeight: 'bold', fontSize: 30, marginTop: '10%', marginBottom: '6%' } }}
             outerContainerStyles={{ backgroundColor: 'rgba(49, 111,244, 1)',
              flexDirection: 'row', borderColor: 'rgba(49, 111,244, 1)',
-             width: '100%', justifyContent: 'space-around', alignItems: 'center' }}
+             width: '100%', height: '15%', justifyContent: 'space-around', alignItems: 'center' }}
             innerContainerStyles={{ justifyContent: 'flex-start' }}/>
         
-        </Card>
     );
   }
 }
--- components/FitStats/FitStatsStyles.js
+++ components/FitStats/FitStatsStyles.js
@@ -1,22 +1,24 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
+    contentContainer: {
+        height: '65%',
         justifyContent: 'space-around',
         alignItems: 'center',
         backgroundColor: 'white',
+        paddingVertical: 0
     },
     chart: {
         width: '85%',
-        height: '80%',
+        height: '100%',
         marginTop: '0%',
         marginBottom: '0%'
     },
     text: {
         fontWeight: 'bold',
         fontSize: 20,
-        color: 'rgba(49, 111,244, 1)'
+        color: 'rgba(49, 111,244, 1)',
+        marginTop: '2%'
     }
 });
 
