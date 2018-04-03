8653e9a0e5085236b7f574a3e3e5113ccec443b2
unknown <jjhernandez94@yahoo.com>
2018-04-03 11:56:21 -0700

    Added splash screen

---
+++ components/MediumQuest/testMedium.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const test = () => {
+
+    return (
+
+        <View>
+            <Text> Testing Medium Page </Text>
+        </View>
+    );
+}
+
+export { test };
