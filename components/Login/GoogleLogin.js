9d71884711c93899013bbddcbd8fc8d8777d9ec3
Mark Mikelonis <mark.mikelonis@gmail.com>
2018-04-20 14:55:17 -0700

    Oauth keys

--- components/Login/GoogleLogin.js
+++ components/Login/GoogleLogin.js
@@ -19,12 +19,12 @@ export default class GoogleLogin extends React.Component {
   _testGoogleLogin = async () => {
     try {
       const result = await Google.logInAsync({
-        androidStandaloneAppClientId:
-          '603386649315-87mbvgc739sec2gjtptl701ha62pi98p.apps.googleusercontent.com',
-        androidClientId: '603386649315-9rbv8vmv2vvftetfbvlrbufcps1fajqf.apps.googleusercontent.com',
+        // androidStandaloneAppClientId:
+        //   '603386649315-87mbvgc739sec2gjtptl701ha62pi98p.apps.googleusercontent.com',
+        androidClientId: '101222014296-vl3io9m5ga8rf8qcii1kas7p2lsjsqb3.apps.googleusercontent.com',
         iosStandaloneAppClientId:
           '603386649315-1b2o2gole94qc6h4prj6lvoiueq83se4.apps.googleusercontent.com',
-        iosClientId: '603386649315-vp4revvrcgrcjme51ebuhbkbspl048l9.apps.googleusercontent.com',
+        iosClientId: '101222014296-c1q5flrh3onjpbhfrmer39t7jn6mhr2t.apps.googleusercontent.com',
         scopes: ['profile', 'email'],
       });
       const { type } = result;
