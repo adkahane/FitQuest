807483565f39948ece3fe735211e6decbe58f95c
psharif <psharif@asu.edu>
2018-04-23 16:21:31 -0700

    added Master changes

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -2,177 +2,179 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../../reducers';
-import { AsyncStorage, StyleSheet, View, TouchableHighlight, ScrollView, Image, Platform } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight, ScrollView, Image, Platform } from 'react-native';
 import { Avatar, List, ListItem } from 'react-native-elements';
-import { Icon, Container, Header, Content, Left, Title, Body, Right, Card, CardItem, Thumbnail, Text } from 'native-base';
+import { Icon, Container, Header, Content, Left, Title, Body, Right, Card, CardItem, Thumbnail } from 'native-base';
 import { CardSection, Input } from '../common';
 import styles from './HomeStyles.js';
 import { NavButtons, DrawerStack} from '../Navigation';
 import { DrawerNavigator } from 'react-navigation';
 import { Spinner } from '../common';
 import { Font } from 'expo';
-// import { User } from '../../server/models/User';
 
 
 class Home extends React.Component {
 
     static navigationOptions = {
-        drawerLabel: () => (
-            <Text
-                style={{ color: '#16a085', fontWeight: 'bold' }}
-            >Home</Text>
-        ),
-      drawerIcon: (
-          <Image source={ require('../../assets/icons/home.png') }
-                 style={{ height: 24, width: 24 }} />
-      )
+        drawerIcon: (
+            <Image source={ require('../../assets/icons/home.png') }
+                   style={{ height: 24, width: 24 }} />
+        )
     }
 
     constructor() {
         super();
         this.state = {
             enabled:true,
-            fontLoaded:false,
-            userLoaded:false,
-            user:{
-                auth_id: '',
-                name:'',
-                email:'',
-                avatar_url:'',
-                points:9333
-              }
+            fontLoaded:false
         };
     }
 
-    async getUser(){
-        console.log("in getUser()");
-        
-        AsyncStorage.getItem('auth_id').then(auth_id => {
-            this.setState({ user: { ...this.state.user, auth_id: auth_id } });
-  
-            console.log('auth_id: ' + this.state.user.auth_id);
-            // const currUser = db.User.find({'auth_id': auth_id});
-            // if(currUser){
-            //     console.log(currUser);
-            // }
-        });
-        AsyncStorage.getItem('name').then(name => {
-            this.setState({ user: { ...this.state.user, name: name } });
-            console.log('name: ' + this.state.user.name);
-        });
-
-        AsyncStorage.getItem('email').then(email => {
-            this.setState({ user: { ...this.state.user, email: email } });
-            console.log('email: ' + this.state.user.email);
-        });
-
-        AsyncStorage.getItem('avatar_url').then(avatar_url => {
-            this.setState({ user: { ...this.state.user, avatar_url: avatar_url } });
-            console.log('avatar_url: ' + this.state.user.avatar_url);
-        });
-        console.log( this.state.userLoaded ); 
-        this.setState({ userLoaded: true });
-        console.log( this.state.userLoaded ); 
-    }
-    
-    async componentWillMount() {
-        await this.getUser();
-        this.setState({ userLoaded: true });
-    }
-
     async componentDidMount() {
-      await Font.loadAsync({
-        'Roboto': require('native-base/Fonts/Roboto.ttf'),
-        'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
-      });
-      this.setState({ fontLoaded: true });
-    }
+        await Font.loadAsync({
+          'Roboto': require('native-base/Fonts/Roboto.ttf'),
+          'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+        });
+        this.setState({ fontLoaded: true });
+      }
 
     render() {
-        if (this.state.fontLoaded && this.state.userLoaded){
-          console.log(this.state);
-          console.log("\n\n\nThis is being rendered\n\n\n");
-          return (
+        const list = [
+            {
+                title: 'User:',
+                subtitle: 'adkahane'
+            },
+            {
+                title: 'Level: ',
+                subtitle: '3'
+            },
+            {
+                title: 'Steps:',
+                subtitle: '8,547'
+            },
+            {
+                title: 'Distance:',
+                subtitle: '4.23 miles'
+            },
+            {
+                title: 'Time',
+                subtitle: '1.5 hours'
+            }
+
+        ]
+        if (this.state.fontLoaded){
+        return (
           <Container>
-              <Header style={{ paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight, backgroundColor: '#aa076b'}}>
-                <Left>
-                  <Icon name="ios-menu" style={{ color: '#fff' }} onPress={() => this.props.navigation.navigate('DrawerOpen')} />
-                </Left>
-                <Body>
-                  <Title style={{ color: '#fff' }}>FitQuest</Title>
-                </Body>
-                <Right />
-              </Header>
-              <Content style={{ height: '100%' }}>
-                <ScrollView scrollEnabled>
-                  <Card style={{ width:'92%', alignSelf: 'center', borderColor: '#52c234', borderWidth: 1, alignItems: 'center',
-                                 marginTop: '4%', paddingBottom:' 78%' }}>
-                    <CardItem style={{ borderColor: '#aa076b', borderWidth: 1, marginTop: '7%', marginBottom:'3%' }}>
-                      <Avatar
-                          xlarge
-                          source={{
-                              uri: this.state.user.avatar_url }}
-                          onPress={() => console.log("Works!")}
-                          activeOpacity={0.7}
-                      />
-                    </CardItem>
-                    <CardItem >
-                      <Body style={{ marginLeft: '20%'}}>
-                        <Text>
-                          <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#aa076b', width: 300}}>
-                            User:&nbsp;&nbsp;
-                          </Text>
-                          <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#52c234', width: 300 }}>
-                            { this.state.user.name }
-                          </Text>
+            <Header style={{ paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight, backgroundColor: '#aa076b'}}>
+              <Left>
+                <Icon name="ios-menu" style={{ color: '#fff' }} onPress={() => this.props.navigation.navigate('DrawerOpen')} />
+              </Left>
+              <Body>
+                <Title style={{ color: '#fff' }}>FitQuest</Title>
+              </Body>
+              <Right />
+            </Header>
+            <Content style={{ height: '100%' }}>
+              {/* <ScrollView contentContainerStyle={{
+                  flexDirection: 'column', width: "100%",
+                  flex: 1, backgroundColor: 'white',
+                  borderColor: 'rgba(44, 244, 250, 1)', justifyContent: 'flex-start',
+                  marginTop: '0%'
+              }} scrollEnabled={this.state.enabled}>
+                  <Avatar
+                      xlarge
+                      source={{
+                          uri: "https://avatars2.githubusercontent.com/u/28679029?s=460&v=4" }}
+                      onPress={() => console.log("Works!")}
+                      activeOpacity={0.7}
+                      containerStyle={{ justifyContent: 'flex-start', alignSelf: 'flex-start', marginTop: '1%', marginBottom: "5%", marginLeft: '5%' }}
+                  />
+                  <View style={{justifyContent: 'flex-start', alignItems: 'flex-start', flexDirection: 'column', marginTop: -15, marginLeft: 0}}>
+                          {
+                              list.map((l, i) => (
+                                  <ListItem
+                                      style={{width: 300}}
+                                      key={i}
+                                      title={l.title}
+                                      subtitle={l.subtitle}
+                                      titleStyle={{ fontSize: 22, fontWeight: 'bold', color: '#aa076b', width: 300}}
+                                      subtitleStyle={{ fontSize: 22, fontWeight: 'bold', color: '#52c234', width: 300}}
+                                  hideChevron={true}
+                                  bottomDivider={false}
+                                  />
+                              ))
+                          }
+                  </View>
+              </ScrollView> */}
+              <ScrollView scrollEnabled>
+                <Card style={{ width:'92%', alignSelf: 'center', borderColor: '#52c234', borderWidth: 1, alignItems: 'center',
+                               marginTop: '4%', paddingBottom:' 78%' }}>
+                  <CardItem style={{ borderColor: '#aa076b', borderWidth: 1, marginTop: '7%', marginBottom:'3%' }}>
+                    <Avatar
+                        xlarge
+                        source={{
+                            uri: "https://avatars2.githubusercontent.com/u/28679029?s=460&v=4" }}
+                        onPress={() => console.log("Works!")}
+                        activeOpacity={0.7}
+                    />
+                  </CardItem>
+                  <CardItem >
+                    <Body style={{ marginLeft: '20%'}}>
+                      <Text>
+                        <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#aa076b', width: 300}}>
+                          User:&nbsp;&nbsp;
                         </Text>
+                        <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#52c234', width: 300 }}>
+                          adkahane
+                        </Text>
+                      </Text>
 
-                        <Text>
-                          <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#aa076b', width: 300}}>
-                            Level:&nbsp;&nbsp;
-                          </Text>
-                          <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#52c234', width: 300 }}>
-                            3
-                          </Text>
+                      <Text>
+                        <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#aa076b', width: 300}}>
+                          Level:&nbsp;&nbsp;
                         </Text>
+                        <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#52c234', width: 300 }}>
+                          3
+                        </Text>
+                      </Text>
 
-                        <Text>
-                          <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#aa076b', width: 300}}>
-                             Steps:&nbsp;&nbsp;
-                          </Text>
-                          <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#52c234', width: 300 }}>
-                            8,547
-                          </Text>
+                      <Text>
+                        <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#aa076b', width: 300}}>
+                           Steps:&nbsp;&nbsp;
                         </Text>
+                        <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#52c234', width: 300 }}>
+                          8,547
+                        </Text>
+                      </Text>
 
-                        <Text>
-                          <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#aa076b', width: 300}}>
-                             Distance:&nbsp;&nbsp;
-                          </Text>
-                          <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#52c234', width: 300 }}>
-                            4.23 miles
-                          </Text>
+                      <Text>
+                        <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#aa076b', width: 300}}>
+                           Distance:&nbsp;&nbsp;
                         </Text>
+                        <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#52c234', width: 300 }}>
+                          4.23 miles
+                        </Text>
+                      </Text>
 
-                        <Text>
-                          <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#aa076b', width: 300}}>
-                             Time:&nbsp;&nbsp;
-                          </Text>
-                          <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#52c234', width: 300 }}>
-                            1.5 hours
-                          </Text>
+                      <Text>
+                        <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#aa076b', width: 300}}>
+                           Time:&nbsp;&nbsp;
                         </Text>
-                      </Body>
-                    </CardItem>
-                  </Card>
-                </ScrollView>
-              </Content>
-            </Container>
-          );
-        }
-      else {
-          return <Spinner />;
-      }
+                        <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#52c234', width: 300 }}>
+                          1.5 hours
+                        </Text>
+                      </Text>
+                    </Body>
+                  </CardItem>
+                </Card>
+              </ScrollView>
+            </Content>
+          </Container>
+        );
+    }
+    else {
+        return <Spinner />;
+    }
     }
 }
 
