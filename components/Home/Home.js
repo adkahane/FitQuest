7f0bd728351e03b6de5d0b2110f45b85fedf7445
Mark Mikelonis <mark.mikelonis@gmail.com>
2018-04-23 13:41:23 -0700

    change home layout components

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -4,7 +4,7 @@ import { createStore } from 'redux';
 import reducers from '../../reducers';
 import { AsyncStorage, StyleSheet, Text, View, TouchableHighlight, ScrollView, Image, Platform } from 'react-native';
 import { Avatar, List, ListItem } from 'react-native-elements';
-import { Icon, Container, Header, Content, Left, Title, Body, Right } from 'native-base';
+import { Icon, Container, Header, Content, Left, Title, Body, Right, Card, CardItem, Thumbnail } from 'native-base';
 import { CardSection, Input } from '../common';
 import styles from './HomeStyles.js';
 import { NavButtons, DrawerStack} from '../Navigation';
@@ -33,7 +33,10 @@ class Home extends React.Component {
                 name:'',
                 email:'',
                 avatar_url:'',
-                points:9333
+                points:9333,
+                steps:'8,547',
+                distance:'4.23 miles',
+                time:'1.5 hours'
               }
         };
     }
@@ -102,61 +105,94 @@ class Home extends React.Component {
 
         ]
         if (this.state.fontLoaded){
-        return (
-        <Container>
-                <Header style={{ paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight, backgroundColor: '#aa076b'}}>
+            return (
+            <Container>
+              <Header style={{ paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight, backgroundColor: '#aa076b'}}>
                 <Left>
-                        <Icon name="ios-menu" style={{ color: '#fff' }} onPress={() => this.props.navigation.navigate('DrawerOpen')} />
+                  <Icon name="ios-menu" style={{ color: '#fff' }} onPress={() => this.props.navigation.navigate('DrawerOpen')} />
                 </Left>
                 <Body>
-                        <Title style={{ color: '#fff' }}>FitQuest</Title>
+                  <Title style={{ color: '#fff' }}>FitQuest</Title>
                 </Body>
                 <Right />
-            </Header>
-            <Content>
-                <ScrollView contentContainerStyle={{
-                    flexDirection: 'column', width: "100%",
-                    flex: 1, backgroundColor: 'white',
-                    borderColor: 'rgba(44, 244, 250, 1)', justifyContent: 'flex-start',
-                    marginTop: '0%'
-                }} scrollEnabled={this.state.enabled}>
-                    <Avatar
-                        xlarge
-                        source={{
-                            uri: this.state.user.avatar_url }}
-                        onPress={() => console.log("Works! " + this.state.user)}
-                        activeOpacity={0.7}
-                        containerStyle={{ justifyContent: 'flex-start', alignSelf: 'flex-start', marginTop: '1%', marginBottom: "5%", marginLeft: '5%' }}
-                    />
-                    <View style={{justifyContent: 'flex-start', alignItems: 'flex-start', flexDirection: 'column', marginTop: -15, marginLeft: 0}}>
-                            {
-                                list.map((l, i) => (
-                                    <ListItem
-                                        style={{width: 300}}
-                                        key={i}
-                                        title={l.title}
-                                        subtitle={l.subtitle}
-                                        titleStyle={{ fontSize: 22, fontWeight: 'bold', color: '#aa076b', width: 300}}
-                                        subtitleStyle={{ fontSize: 22, fontWeight: 'bold', color: '#52c234', width: 300}}
-                                    hideChevron={true}
-                                    bottomDivider={false}
-                                    />
-                                ))
-                            }
-                    </View>
+              </Header>
+              <Content style={{ height: '100%' }}>
+               
+                <ScrollView scrollEnabled>
+                  <Card style={{ width:'92%', alignSelf: 'center', borderColor: '#52c234', borderWidth: 1, alignItems: 'center',
+                                 marginTop: '4%', paddingBottom:' 78%' }}>
+                                 {/* marginRight:'4%', marginLeft:'4%' }}> */}
+                    <CardItem style={{ borderColor: '#aa076b', borderWidth: 1, marginTop: '7%', marginBottom:'3%' }}>
+                      <Avatar
+                          xlarge
+                          source={{
+                              uri: "https://avatars2.githubusercontent.com/u/28679029?s=460&v=4" }}
+                          onPress={() => console.log("Works!")}
+                          activeOpacity={0.7}
+                      />
+                    </CardItem>
+                    <CardItem >
+                      <Body style={{ marginLeft: '20%'}}>
+                        <Text>
+                          <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#aa076b', width: 300}}>
+                            User:&nbsp;&nbsp;
+                          </Text>
+                          <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#52c234', width: 300 }}>
+                            {this.state.user.name}
+                          </Text>
+                        </Text>
+    
+                        <Text>
+                          <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#aa076b', width: 300}}>
+                            Level:&nbsp;&nbsp;
+                          </Text>
+                          <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#52c234', width: 300 }}>
+                            3
+                          </Text>
+                        </Text>
+    
+                        <Text>
+                          <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#aa076b', width: 300}}>
+                             Steps:&nbsp;&nbsp;
+                          </Text>
+                          <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#52c234', width: 300 }}>
+                            this.state.user.steps
+                          </Text>
+                        </Text>
+    
+                        <Text>
+                          <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#aa076b', width: 300}}>
+                             Distance:&nbsp;&nbsp;
+                          </Text>
+                          <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#52c234', width: 300 }}>
+                            {this.state.user.distance}
+                          </Text>
+                        </Text>
+    
+                        <Text>
+                          <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#aa076b', width: 300}}>
+                             Time:&nbsp;&nbsp;
+                          </Text>
+                          <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#52c234', width: 300 }}>
+                            {this.state.user.time}
+                          </Text>
+                        </Text>
+                      </Body>
+                    </CardItem>
+                  </Card>
                 </ScrollView>
-            </Content>
-        </Container>
-
-
-
-
-        );
+              </Content>
+            </Container>
+    
+    
+    
+    
+            );
+        }
+        else {
+            return <Spinner />;
+        }
+        }
     }
-    else {
-        return <Spinner />;
-    }
-    }
-}
-
-export default Home;
+    
+    export default Home;
