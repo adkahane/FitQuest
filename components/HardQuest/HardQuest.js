74ce1cf37e722fb0d1c468c1015275fc6eb68442
unknown <jjhernandez94@yahoo.com>
2018-04-24 13:27:38 -0700

    Modulated quest arrays

--- components/HardQuest/HardQuest.js
+++ components/HardQuest/HardQuest.js
@@ -1,6 +1,15 @@
 import React from 'react';
-import { Image, Text } from 'react-native';
+import { Image, Text, Dimensions } from 'react-native';
 import { Container, Header, Content, Card, CardItem, Thumbnail, Button, Icon, Left, Body, Right } from 'native-base';
+import hardQuest from '../HardQuest/hard-quest.json';
+import { Map } from '../common/CardMap.js';
+
+let { width, height } = Dimensions.get('window');
+let latitude = 	37.868243; 
+let longitude = -121.926388;
+const ASPECT_RATIO = width / height;
+const LATITUDE_DELTA = 0.02280;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 export default class HardQuest extends React.Component {
   render() {
@@ -14,26 +23,24 @@ export default class HardQuest extends React.Component {
                 </Body>
             </CardItem> 
             <CardItem cardBody>
-              <Image source={{uri: 'https://camo.githubusercontent.com/689a96e5a1b1522bf9da9001ac66b00dad621ac5/687474703a2f2f656d63636f6e76696c6c652e636f6d2f506f6c796c696e652f647573747967726f6f76652e706e67'}} style={{height: 200, width: 100, flex: 1}}/>
+              <Map
+                location={{ 
+                latitude: latitude,
+                longitude: longitude,
+                latitudeDelta: LATITUDE_DELTA,
+                longitudeDelta: LONGITUDE_DELTA}}
+                polylines= { hardQuest } /> 
             </CardItem>
             <CardItem>
               <Left>
                 <Button transparent>
                   <Icon active name="walk" />
-                  <Text>&nbsp;12,000 Steps &nbsp;&nbsp;&nbsp;&nbsp;</Text>
+                  <Text>&nbsp;Steps: 9,000&nbsp;&nbsp;&nbsp;</Text>
                   <Icon active name="stopwatch" />
-                  <Text>&nbsp;Average Time: 60:00</Text>
-                </Button>
+                  <Text>&nbsp;Time: 105:00</Text>
+                  <Text>&nbsp;Climb: 2,644 ft</Text>
+                  </Button>
               </Left>
-           {/*   <Body>
-                <Button transparent>
-                  <Icon active name="chatbubbles" />
-                  <Text>4 Comments</Text>
-                </Button>
-              </Body>
-              <Right>
-                <Text>11h ago</Text>
-           </Right> */}
             </CardItem>
           </Card>
         </Content>
