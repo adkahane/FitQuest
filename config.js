ae7b7da075a6b6fb15bb707fe2a06580b1eb8e1c
Mark Mikelonis <34202562+Mark-Mikelonis@users.noreply.github.com>
2018-04-26 15:06:11 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+
+const androidClient = {
+    key:'101222014296-vl3io9m5ga8rf8qcii1kas7p2lsjsqb3.apps.googleusercontent.com'
+}
+
+const iosClient = {
+    key: '101222014296-24greqvj9l32brissi30k5cr0thrio68.apps.googleusercontent.com'
+}
+module.exports = {
+    androidClient: androidClient,
+    iosClient: iosClient
+}
